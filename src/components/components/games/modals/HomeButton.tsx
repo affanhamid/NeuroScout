5bf04e74c2ec5cd20fd13ecf967e9e2b031d27c0
affanhamid <affanhamid007@gmail.com>
2024-10-17 17:50:20 +0100

    Added back button

---
+++ src/components/components/games/modals/HomeButton.tsx
@@ -0,0 +1,9 @@
+const HomeButton = () => {
+  return (
+    <button className="fixed top-5 left-5 text-white flex items-center gap-2 text-xl z-[1000]">
+      Home
+    </button>
+  );
+};
+
+export default HomeButton;
