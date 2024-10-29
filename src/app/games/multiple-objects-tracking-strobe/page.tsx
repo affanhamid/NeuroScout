9fe55b914fd2a45ee49a9f4ad417589bda900fe0
Spirity123 <146657544+Spirity123@users.noreply.github.com>
2024-10-29 20:37:29 +0000

    trying flash

--- src/app/games/multiple-objects-tracking-strobe/page.tsx
+++ src/app/games/multiple-objects-tracking-strobe/page.tsx
@@ -4,7 +4,7 @@ import StroboscopicMOTGame from "@/components/components/MOT/MOTStroboscopic";
 const page = () => {
   return (
     <main>
-      <StroboscopicMOTGame strobeA={1000} strobeB={100} isRandom={false} />
+      <StroboscopicMOTGame strobeA={1000} strobeB={10} isRandom={false} />
     </main>
   );
 };
