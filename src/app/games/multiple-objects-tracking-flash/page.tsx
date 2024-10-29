21a0a69bb1d391622e52cbe160f495c9047e9157
Spirity123 <146657544+Spirity123@users.noreply.github.com>
2024-10-29 20:43:11 +0000

    fixed strobe and flash

--- src/app/games/multiple-objects-tracking-flash/page.tsx
+++ src/app/games/multiple-objects-tracking-flash/page.tsx
@@ -4,7 +4,7 @@ import StroboscopicMOTGame from "@/components/components/MOT/MOTStroboscopic";
 const page = () => {
   return (
     <main>
-      <StroboscopicMOTGame strobeA={1000} strobeB={1000} isRandom={true} />
+      <StroboscopicMOTGame strobeA={1000} strobeB={100} isRandom={true} />
     </main>
   );
 };
