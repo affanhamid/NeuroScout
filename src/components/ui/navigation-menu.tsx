c98a84246cd0f5272c283cfefe0d7d0424fea150
Spirity123 <146657544+Spirity123@users.noreply.github.com>
2024-11-16 05:03:36 +0000

    Added new game thumbnails

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -14,7 +14,7 @@ const NavigationMenu = React.forwardRef<
     ref={ref}
     className={cn(
       "relative z-10 flex max-w-max flex-1 items-center justify-center",
-      className,
+      className
     )}
     {...props}
   >
@@ -32,7 +32,7 @@ const NavigationMenuList = React.forwardRef<
     ref={ref}
     className={cn(
       "group flex flex-1 list-none items-center justify-center space-x-1",
-      className,
+      className
     )}
     {...props}
   />
@@ -42,7 +42,7 @@ NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName;
 const NavigationMenuItem = NavigationMenuPrimitive.Item;
 
 const navigationMenuTriggerStyle = cva(
-  "group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-zinc-100 hover:text-zinc-900 focus:bg-zinc-100 focus:text-zinc-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-zinc-100/50 data-[state=open]:bg-zinc-100/50 dark:bg-zinc-950 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:focus:bg-zinc-800 dark:focus:text-zinc-50 dark:data-[active]:bg-zinc-800/50 dark:data-[state=open]:bg-zinc-800/50",
+  "group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-zinc-100 hover:text-zinc-900 focus:bg-zinc-100 focus:text-zinc-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-zinc-100/50 data-[state=open]:bg-zinc-100/50 dark:bg-zinc-950 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:focus:bg-zinc-800 dark:focus:text-zinc-50 dark:data-[active]:bg-zinc-800/50 dark:data-[state=open]:bg-zinc-800/50"
 );
 
 const NavigationMenuTrigger = React.forwardRef<
@@ -72,7 +72,7 @@ const NavigationMenuContent = React.forwardRef<
     ref={ref}
     className={cn(
       "left-0 top-0 w-full data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 md:absolute md:w-auto",
-      className,
+      className
     )}
     {...props}
   />
@@ -89,7 +89,7 @@ const NavigationMenuViewport = React.forwardRef<
     <NavigationMenuPrimitive.Viewport
       className={cn(
         "origin-top-center relative mt-1.5 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden rounded-md border border-zinc-200 bg-white text-zinc-950 shadow data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 md:w-[var(--radix-navigation-menu-viewport-width)] dark:border-zinc-800 dark:bg-zinc-950 dark:text-zinc-50",
-        className,
+        className
       )}
       ref={ref}
       {...props}
@@ -107,7 +107,7 @@ const NavigationMenuIndicator = React.forwardRef<
     ref={ref}
     className={cn(
       "top-full z-[1] flex h-1.5 items-end justify-center overflow-hidden data-[state=visible]:animate-in data-[state=hidden]:animate-out data-[state=hidden]:fade-out data-[state=visible]:fade-in",
-      className,
+      className
     )}
     {...props}
   >
@@ -128,7 +128,7 @@ const ListItem = React.forwardRef<
           ref={ref}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className,
+            className
           )}
           {...props}
         >
@@ -142,39 +142,6 @@ const ListItem = React.forwardRef<
   );
 });
 ListItem.displayName = "ListItem";
-
-const games = [
-  {
-    title: "Trace & Track",
-    href: "/games/tnt",
-    description:
-      "A game where the player has to track multiple objects bouncing around in an animation",
-  },
-  {
-    title: "Trace & Track Flash",
-    href: "/games/tnt-flash",
-    description:
-      "A game where the player has to track multiple objects bouncing around in an animation with flash",
-  },
-  {
-    title: "Trace & Track Strobe",
-    href: "/games/tnt-strobe",
-    description:
-      "A game where the player has to track multiple objects bouncing around in an animation with stroboscopic vision",
-  },
-  // {
-  //   title: "Memory Grid",
-  //   href: "/games/memory-grid",
-  //   description:
-  //     "A game where the player has to memorize locations and patterns of disappearing objects in a grid",
-  // },
-  // {
-  //   title: "Anticipation Game",
-  //   href: "/games/anticipation-game",
-  //   description:
-  //     "A game where the player has to anticipate the trajectory of multiple balls moving around",
-  // },
-];
 
 const ShadNavbar = () => {
   return (
@@ -209,18 +176,6 @@ const ShadNavbar = () => {
               </ListItem>
               <ListItem href="/contact-us" title="Contact Us">
                 Contact us for more information
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>
-            <span className="text-black">Games</span>
-          </NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px]lg:w-[600px] ">
-              <ListItem href="/games" title="Gallery">
-                Take a look at the games we provide
               </ListItem>
             </ul>
           </NavigationMenuContent>
