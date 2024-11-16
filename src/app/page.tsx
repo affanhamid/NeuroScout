897c69f34ce97193a690519de57ca862bd44b0c7
Spirity123 <146657544+Spirity123@users.noreply.github.com>
2024-11-16 03:43:37 +0000

    Added research card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export const Card = ({ heading, text }: { heading: string; text: string }) => {
   return (
     <div className="bg-dark w-[320px] aspect-[0.9] rounded-lg text-white text-center py-10">
       <h3 className="">{heading}</h3>
-      <p className="text-lg">{text}</p>
+      <p>{text}</p>
     </div>
   );
 };
@@ -15,9 +15,9 @@ export default function Home() {
   return (
     <main>
       <Navbar />
-      <section className="bg-hero h-[75vh] mt-20 px-40">
+      <section className="bg-hero h-[75vh] mt-20">
         <div className="flex h-3/4 items-center justify-between">
-          <div className="text-white flex flex-col gap-5 w-[45%]">
+          <div className="text-white flex flex-col gap-5 w-[50%]">
             <h1>
               The future of <br /> football talent acquisition
             </h1>
@@ -28,11 +28,11 @@ export default function Home() {
             <button className="bg-white text-primary">Get Started</button>
           </div>
           <div>
-            <div className="bg-dark w-[700px] rounded-xl aspect-video"></div>
+            <div className="bg-dark w-[550px] rounded-xl aspect-video"></div>
           </div>
         </div>
       </section>
-      <section className="px-40 py-20">
+      <section className="py-20">
         <h2 className="mb-20">
           What is <span className="text-primary">NeuroScout</span>
         </h2>
@@ -40,6 +40,19 @@ export default function Home() {
           <Card heading="Perform cognitive Tests" text="" />
           <Card heading="Gain in-depth analysis" text="" />
           <Card heading="Pick out talent and train" text="" />
+        </div>
+
+        <div className="py-10 flex rounded-lg justify-between px-10 bg-[#CDD4DD] mt-20">
+          <div className="flex flex-col w-1/2">
+            <h3 className="text-3xl mb-10">Follow our research</h3>
+            <p>
+              Understand our research and findings about cognitive assessments
+              in football. Understand our research and findings about cognitive
+              assessments in football. Understand our research and findings
+              about cognitive assessments in football
+            </p>
+          </div>
+          <div className="w-60 bg-white aspect-[3/4] rounded-lg"></div>
         </div>
       </section>
       <section></section>
