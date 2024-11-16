b09e9be02c48a26e68d817331fbb6b5e710de976
Spirity123 <146657544+Spirity123@users.noreply.github.com>
2024-11-16 03:34:52 +0000

    added what is neuroscout section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,20 @@
 import Footer from "@/components/components/Footer";
 import Navbar from "@/components/components/Navbar";
 
+export const Card = ({ heading, text }: { heading: string; text: string }) => {
+  return (
+    <div className="bg-dark w-[320px] aspect-[0.9] rounded-lg text-white text-center py-10">
+      <h3 className="">{heading}</h3>
+      <p className="text-lg">{text}</p>
+    </div>
+  );
+};
+
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <section className="bg-hero h-[75vh] mt-20 px-32">
+      <section className="bg-hero h-[75vh] mt-20 px-40">
         <div className="flex h-3/4 items-center justify-between">
           <div className="text-white flex flex-col gap-5 w-[45%]">
             <h1>
@@ -23,7 +32,16 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section></section>
+      <section className="px-40 py-20">
+        <h2 className="mb-20">
+          What is <span className="text-primary">NeuroScout</span>
+        </h2>
+        <div className="flex justify-between">
+          <Card heading="Perform cognitive Tests" text="" />
+          <Card heading="Gain in-depth analysis" text="" />
+          <Card heading="Pick out talent and train" text="" />
+        </div>
+      </section>
       <section></section>
       <section></section>
       <div className="mt-40"></div>
