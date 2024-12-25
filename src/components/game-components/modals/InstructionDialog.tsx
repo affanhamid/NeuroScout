34e7389e8b3eae4ddcdffdb275c15c7c56f778c0
Dev Patel <dev.patel3@outlook.com>
2024-12-25 01:24:25 +0000

    removed redundant code in InstructionDialog

--- src/components/game-components/modals/InstructionDialog.tsx
+++ src/components/game-components/modals/InstructionDialog.tsx
@@ -80,7 +80,6 @@ const InstructionDialog = ({
           <Carousel instructions={instructions} step={step} />
 
           {/* Navigation Buttons */}
-          {step < instructions.length ? (
             <div className="absolute left-0 right-0 top-[50%] transform -translate-y-[50%] flex justify-between">
               <button
                 onClick={() => handleClick(-1)}
@@ -124,22 +123,6 @@ const InstructionDialog = ({
                 </svg>
               </button>
             </div>
-          ) : (
-            <div className="text-white flex flex-col gap-5 items-center">
-          <button
-            className="rounded-full px-5 py-2 bg-green-700 hover:bg-green-600 active:bg-green-600 text-white"
-            onClick={onStart}
-          >
-            Start Game
-          </button>
-          <button
-            className="rounded-full px-5 py-2 bg-gray-700 hover:bg-gray-600 active:bg-gray-800 text-white"
-            onClick={() => setStep(0)}
-          >
-            Show Instructions
-          </button>
-        </div>   
-          )}
         </div>
       ) : (
         <div className="text-white flex flex-col gap-5 items-center">
