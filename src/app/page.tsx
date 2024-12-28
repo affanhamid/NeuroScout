bfacc6d0e5ae2ba7c4c0ac46bf0aa59548e431ef
Dev Patel <dev.patel3@outlook.com>
2024-12-28 19:51:29 +0000

    added logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,22 @@ import Footer from "../components/ui/Footer";
 import Gallery from "../components/ui/Gallery";
 import { GameTypeWithId } from "@/types";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="pt-24 px-20">
+    <main className="relative">
+      <section className="absolute top-0 right-0 p-4">
+        <div className="w-96 h-40 relative">
+          <Image
+            src="https://utfs.io/f/OA98Upl9HVAuvpSSFXFEoOw4kpIy6bEPGclZf1CmNaUt7H5L"
+            alt="NeuroScout Logo"
+            fill
+            priority
+            className="object-contain"
+          />
+        </div>
+      </section>
       <section className="mb-16">
         <h1 className="text-center text-7xl mb-5 text-primary">NeuroScout</h1>
         <p className="text-3xl text-bold text-center">
