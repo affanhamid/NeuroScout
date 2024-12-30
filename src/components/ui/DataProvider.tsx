591778a0b387c478bfe52aa5e2a7b2aed331b779
Spirity123 <146657544+Spirity123@users.noreply.github.com>
2024-12-30 21:34:46 +0000

    added cache

--- src/components/ui/DataProvider.tsx
+++ src/components/ui/DataProvider.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 async function fetchData<T>(url: string): Promise<T> {
-  const response = await fetch(url);
+  const response = await fetch(url, { cache: "no-store" });
   if (!response.ok) {
     throw new Error("Failed to fetch data" + response);
   }
