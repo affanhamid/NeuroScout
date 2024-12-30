1c9540bea14f12cfeadaded3ab98e03ba1d78d36
Spirity123 <146657544+Spirity123@users.noreply.github.com>
2024-12-30 21:00:24 +0000

    added web analytics and speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,19 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "NeuroScout",
   description:
-    "NeuroScout is a football talent-acquisition platform through cognitive assessments",
+    "NeuroScout is a football talent-acquisition platform through cognitive assessments"
 };
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default async function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
@@ -20,6 +22,8 @@ export default async function RootLayout({
       <body className={inter.className}>
         <main>{children}</main>
       </body>
+      <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
