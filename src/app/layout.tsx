8e62adbf655080c45791a520cb4a2b52fe25cb73
Spirity123 <146657544+Spirity123@users.noreply.github.com>
2025-01-01 16:19:31 +0000

    added favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
-// import { Analytics } from "@vercel/analytics/react";
-// iimport { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "NeuroScout",
@@ -19,10 +19,40 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Favicons */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+
+        {/* Theme Color */}
+        <meta name="theme-color" content="#ffffff" />
+
+        {/* Metadata */}
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body className={inter.className}>
         <main>{children}</main>
+        <Analytics />
+        <SpeedInsights />
       </body>
-
     </html>
   );
 }
