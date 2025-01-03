9f242e9216ade5c56c59cd9b19c6d3fdda4bd8bb
Dev Patel <dev.patel3@outlook.com>
2025-01-03 19:38:57 +0000

    changed redirect text

--- src/components/game-components/modals/ThankyouDialog.tsx
+++ src/components/game-components/modals/ThankyouDialog.tsx
@@ -21,8 +21,8 @@ const ThankYouDialog = ({ redirectLink = "/" }: { redirectLink?: string }) => {
   return (
     <div className="bg-game-background absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center">
       <div className="text-white border-white border-2 rounded-xl w-1/2 h-1/2 flex flex-col items-center justify-center py-32">
-        <h2 className="text-5xl pb-10">Thank You!</h2>
-        <p className="text-xl pb-10">Redirecting to the next game...</p>
+        <h2 className="text-5xl pb-10">Thank You for playing!</h2>
+        <p className="text-xl pb-10">Redirecting you now...</p>
       </div>
     </div>
   );
