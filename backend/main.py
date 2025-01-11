6d120100edd12555a1fe65630b12562903a87e2d
Spirity123 <67050881+affanhamid@users.noreply.github.com>
2025-01-11 19:59:28 +0000

    added support for env variables

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,6 @@ import os
 from dotenv import load_dotenv
 from lib.db import DB
 
-load_dotenv(".env.local")
+load_dotenv(".env")
 db = DB(os.getenv("DOCUMENT_URI"))
 print(db.get_collection("test"))
