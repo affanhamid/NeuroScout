d6bd1e34ba90ab3924861b7ee9f80c15bb16a73a
affanhamid <affanhamid007@gmail.com>
2025-04-01 18:39:12 +0100

    some minor formatting in thank you page

--- frontend/src/components/game-components/modals/ThankyouDialog.tsx
+++ frontend/src/components/game-components/modals/ThankyouDialog.tsx
@@ -24,11 +24,11 @@ const ThankYouDialog = ({ redirectLink = "/" }: { redirectLink?: string }) => {
   return (
     <div className="bg-game-background absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center">
       <div className="text-white border-white border-2 rounded-xl w-1/2 h-1/2 flex flex-col items-center justify-center py-32">
-        <h2 className="text-5xl pb-10 flex flex-col gap-3">
+        <h2 className="text-5xl text-center pb-10 flex flex-col gap-3">
           <span>Thank you for playing!</span>
           {redirectLink.includes("test") ? (
             <p className="text-xl pb-10">
-              Uploading results and Redirecting to organisation page...
+              Uploading results and redirecting to organisation page...
             </p>
           ) : (
             <p className="text-xl pb-10">Redirecting to next game...</p>
